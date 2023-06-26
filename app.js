36f54d4832ad8cec31977f44eeee7b2c9a0405cc
hamza <hamza.prolink@gmail.com>
2023-06-26 11:18:58 +0500

    updated mongoose

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
 //     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
+//     "GET, POST, PUT, PATCH, DELETE OPTIONS"
 //   );
 //   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 //   next();
